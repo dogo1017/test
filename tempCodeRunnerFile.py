dbed38907e115a875ddb881a1ff1d206ae1296d2
dogo1017 <douglas.london@ucas-edu.net>
2026-01-23 09:37:06 -0700

    Refactor menu function and add current text display

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+            print(f"Current Text: {''.join(desc)}") 
--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-"""
 import os
 import msvcrt
 
@@ -8,21 +7,31 @@ def menu(options):
     while True:
         os.system('cls')
         for i, option in enumerate(options):
-            prefix = "> " if i == index else "  "
-            print(prefix + option)
+            prefix = "> " if i == index else "  " 
+            if i == 0:
+                print(f"{prefix}{option}: {''.join(desc)}")
+            else:
+                print(f"{prefix}{option}")
+
         key = msvcrt.getch()
+
         if key in (b'\x00', b'\xe0'):
             key = msvcrt.getch()
-        if key == b"H":
-            index = (index - 1) % len(options)
-        elif key == b"P":
-            index = (index + 1) % len(options)
-        elif key == b"\r" and index == 0:
-            return index  
-        if index == 0:
-            text = input("Press enter to choose different option")
-              
-        
-        
-menu(["text","return"])
-"""
+            if key == b'H': 
+                index = (index - 1) % len(options)
+            elif key == b'P':
+                index = (index + 1) % len(options)
+        elif key == b'\r':
+            if index == 0:
+
+                pass 
+            else:
+                return index
+        elif key == b'\b':
+            if index == 0 and desc:
+                desc.pop()
+        else:
+            if index == 0:
+                desc.append(key.decode('utf-8'))
+
+menu(["one", "two"])
