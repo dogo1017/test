0cd02d3da501cac94e4245ebfb4500b3616cc073
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2026-01-27 09:52:01 -0700

    cool

--- inventory_manager.py
+++ inventory_manager.py
@@ -9,24 +9,17 @@ def main():
     while True:
         choice = menu(options)
         if choice == 0:
-            add_character.add_menu()
+            add_items()
         elif choice == 1:
             if selected_character == "":
-                skill_manager.skill_menu()
+                remove_items()
             else:
                 print("Please select a character before entering this function.")
                 input("Press Enter to continue...")
                 continue
         elif choice == 2:
             if selected_character != "":
-                inventory_manager.inventory_menu()
-            else:
-                print("Please select a character before entering this function.")
-                input("Press Enter to continue...")
-                continue
-        elif choice == 3:
-            if selected_character != "":
-                attribute_manager.attribute_menu()
+                menu()
             else:
                 print("Please select a character before entering this function.")
                 input("Press Enter to continue...")
