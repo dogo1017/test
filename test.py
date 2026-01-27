3eca62c67121d73a0e37444c92ddac1cec0c9ef9
dogo1017 <douglas.london@ucas-edu.net>
2026-01-27 09:23:52 -0700

    test code update

--- test.py
+++ test.py
@@ -4,40 +4,41 @@ import msvcrt
 """
 def menu(options):
     desc = []
-    index = 0
-    while True:
-        os.system('cls')
-        for i, option in enumerate(options):
-            prefix = "> " if i == index else "  " 
-            if i == 0:
-                print(f"{prefix}{option}: {''.join(desc)}")
-            else:
-                print(f"{prefix}{option}")
+        index = 0
+            while True:
+                    os.system('cls')
+                            for i, option in enumerate(options):
+                                        prefix = "> " if i == index else "  " 
+                                                    if i == 0:
+                                                                    print(f"{prefix}{option}: {''.join(desc)}")
+                                                                                else:
+                                                                                                print(f"{prefix}{option}")
 
-        key = msvcrt.getch()
+                                                                                                        key = msvcrt.getch()
 
-        if key in (b'\x00', b'\xe0'):
-            key = msvcrt.getch()
-            if key == b'H': 
-                index = (index - 1) % len(options)
-            elif key == b'P':
-                index = (index + 1) % len(options)
-        elif key == b'\r':
-            if index == 0:
+                                                                                                                if key in (b'\x00', b'\xe0'):
+                                                                                                                            key = msvcrt.getch()
+                                                                                                                                        if key == b'H': 
+                                                                                                                                                        index = (index - 1) % len(options)
+                                                                                                                                                                    elif key == b'P':
+                                                                                                                                                                                    index = (index + 1) % len(options)
+                                                                                                                                                                                            elif key == b'\r':
+                                                                                                                                                                                                        if index == 0:
 
-                pass 
-            else:
-                return index
-        elif key == b'\b':
-            if index == 0 and desc:
-                desc.pop()
-        else:
-            if index == 0:
-                desc.append(key.decode('utf-8'))
+                                                                                                                                                                                                                        pass 
+                                                                                                                                                                                                                                    else:
+                                                                                                                                                                                                                                                    return index
+                                                                                                                                                                                                                                                            elif key == b'\b':
+                                                                                                                                                                                                                                                                        if index == 0 and desc:
+                                                                                                                                                                                                                                                                                        desc.pop()
+                                                                                                                                                                                                                                                                                                else:
+                                                                                                                                                                                                                                                                                                            if index == 0:
+                                                                                                                                                                                                                                                                                                                            desc.append(key.decode('utf-8'))
 
-menu(["one", "two"])"""
+menu(["one", "two"])
 
-def menu2(options, writable):
+"""
+"""def menu2(options, writable):
     desc = []
     for i in writable:
         desc.append([])
@@ -71,4 +72,47 @@ def menu2(options, writable):
             if index in writable:
                 desc[index].append(key.decode('utf-8'))
 
-menu2(["description", "effect", "target", "return"], [0,1,2])
+menu2(["description", "effect", "target", "return"], [0,1,2])"""
+
+
+def menu(options, **mod):
+    writable = mod.get('writable', [])
+    toggle = mod.get('toggle', [])
+    default_TF = mod.get('default_vals', [])
+    states = {i: 0 for i in toggle} 
+    text_data = {i: [] for i in writable}
+    index = 0
+    while True:
+        os.system('cls')
+        for i, option in enumerate(options):
+            prefix = "> " if i == index else "  " 
+            if i in writable:
+                print(f"{prefix}{option}: {''.join(text_data[i])}")
+            else:
+                print(f"{prefix}{option}")
+
+        key = msvcrt.getch()
+
+        if key in (b'\x00', b'\xe0'):
+            key = msvcrt.getch()
+            if key == b'H': 
+                index = (index - 1) % len(options)
+            elif key == b'P':
+                index = (index + 1) % len(options)
+        elif key == b'\r':
+            if index in writable:
+                return {'index': index, 'writable': text_data}
+            elif index in toggle:
+                states[index] *= -1
+            else:
+                return index
+        elif key == b'\b':
+            if index in writable and text_data[index]:
+                text_data[index].pop()
+        else:
+            if index in writable:
+                text_data[index].append(key.decode('utf-8'))
+input = menu(["Hi", "bored", "Dont know"], writable = [1,2])
+
+stuff = input['writable'][1]
+print(stuff)
